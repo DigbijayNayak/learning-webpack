151d9fcad8140b82ce52c0d1f288812dcc49ab6d
DigbijayNayak555 <digbijay.nayak@peopletech.com>
2024-11-10 09:12:50 +0530

    Load images with webpack

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-const Greeting = () => <h1>Hello from React</h1>;
+const Greeting = () => {
+  return (
+    <div>
+      <h1>Hello from React</h1>
+      <div id="image"></div>
+    </div>
+  );
+};
 
 const rootElement = document.getElementById('target');
 const root = ReactDOM.createRoot(rootElement);
