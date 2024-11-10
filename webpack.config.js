b072424e59ebac0c54e5e4a7aae1d5cce16ac092
DigbijayNayak555 <digbijay.nayak@peopletech.com>
2024-11-10 11:17:42 +0530

    Prepare code for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -18,6 +19,9 @@ module.exports = {
   //     chunks: 'all',
   //   },
   // },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()],
+  },
   plugins: [new HtmlWebpackPlugin()],
   devServer: {
     static: {
